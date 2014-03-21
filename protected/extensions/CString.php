7d26d1d459254466aef18651d070f4dacaf23bb9
Barta Akos <akos@netpositive.hu>
2014-03-21 16:09:22 +0100

    CString mb

--- protected/extensions/CString.php
+++ protected/extensions/CString.php
@@ -5,17 +5,17 @@
 
 class CString
 {
-  static function truncate($string, $limit, $break = " ", $pad = "...")
-  {
-    // return with no change if string is shorter than $limit
-    if (strlen($string) <= $limit) {
-      return $string;
+    static function truncate($string, $limit, $break = " ", $pad = "...")
+    {
+        // return with no change if string is shorter than $limit
+        if (mb_strlen($string, 'UTF-8') <= $limit) {
+            return $string;
+        }
+        $string = mb_substr($string, 0, $limit, 'UTF-8');
+        if (false !== ($breakpoint = mb_strrpos($string, $break, null, 'UTF-8'))) {
+            $string = mb_substr($string, 0, $breakpoint, 'UTF-8');
+        }
+        return $string . $pad;
     }
-    $string = substr($string, 0, $limit);
-    if (false !== ($breakpoint = strrpos($string, $break))) {
-      $string = substr($string, 0, $breakpoint);
-    }
-    return $string . $pad;
-  }
 
 }
